8c8e2259eb0bc1e65dc1f474a7554055d8ea13be
alexbasista <abasista@hashicorp.com>
2024-09-23 10:16:51 -0400

    bump versions

--- versions.tf
+++ versions.tf
@@ -2,16 +2,16 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-  required_version = "~> 1.9"
+  required_version = ">= 1.9"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.39"
+      version = "~> 5.43"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 5.39"
+      version = "~> 5.43"
     }
     random = {
       source  = "hashicorp/random"
@@ -19,5 +19,3 @@ terraform {
     }
   }
 }
-
-
