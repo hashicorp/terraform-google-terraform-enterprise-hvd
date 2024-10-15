8a019e94caf504d836770cc4373c6feb0875fcba
alexbasista <abasista@hashicorp.com>
2024-10-15 09:12:40 -0400

    add redis cmek data source

--- redis.tf
+++ redis.tf
@@ -1,6 +1,9 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
+#------------------------------------------------------------------------------
+# Cloud Memorystore for Redis
+#------------------------------------------------------------------------------
 resource "google_redis_instance" "tfe" {
   count = var.tfe_operational_mode == "active-active" ? 1 : 0
   
@@ -14,4 +17,21 @@ resource "google_redis_instance" "tfe" {
   authorized_network      = data.google_compute_network.vpc.self_link
   connect_mode            = var.redis_connect_mode
   labels                  = var.common_labels
+}
+
+#------------------------------------------------------------------------------
+# KMS customer managed encryption key (CMEK)
+#------------------------------------------------------------------------------
+data "google_kms_key_ring" "redis" {
+  count = var.redis_kms_keyring_name != null ? 1 : 0
+
+  name     = var.redis_kms_keyring_name
+  location = data.google_client_config.current.region
+}
+
+data "google_kms_crypto_key" "redis" {
+  count = var.redis_kms_cmek_name != null ? 1 : 0
+
+  name     = var.redis_kms_cmek_name
+  key_ring = data.google_kms_key_ring.redis[0].id
 }
